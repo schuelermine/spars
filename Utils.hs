8cb27fa17a26ca32559e69ab64d64519f954f54f
anselmschueler <git@anselmschueler.com>
2020-08-19 20:54:51 +0200

    something

--- Utils.hs
+++ Utils.hs
@@ -9,12 +9,12 @@ crossClasses :: (Ord k, Ord a, Ord b) => (a -> b -> c) -> Set.Set (a, k) -> Set.
 crossClasses set1 set2 =
     let map1 = groupS' set1
         map2 = groupS' set2
-        fold key setA set0 =
+        f key setA set0 =
             let setB = Map.findWithDefault Set.empty key map2
                 setC = Set.cartesianProduct setA setB
                 set_ = Set.map (, key) setC
             in  Set.union set0 set_
-    in  Map.foldrWithKey fold Set.empty map1
+    in  Map.foldrWithKey f Set.empty map1
 
 groupS' :: (Ord k, Ord a) => Set.Set (a, k) -> Map.Map k (Set.Set a)
 groupS' = Map.unionsWith (Set.union) . Set.map (\(a, b) -> Map.singleton b (Set.singleton a))
